caaba3de411f3395c74bad04f3051f4188120362
hhshhhskk <hhshhhskk@naver.com>
2023-04-21 19:53:18 +0900

    useStaticQuery, PageQuery

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,12 +3,20 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    description: "DevStickers"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog-posts`,
+      }
+    }
+  ],
 }
 
 export default config
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,29 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
+import { PageProps, graphql } from "gatsby"
 
-const BlogPage = () => {
+const BlogPage = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
     return (
         <Layout title="Blog">
-            <p>Blog</p>
+            <ul>
+                {data.allFile.nodes.map((file, index) => (
+                    <li key={index}>{file.name}</li>
+                ))}
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+query BlogTitles {
+    allFile {
+      nodes{
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />
 
