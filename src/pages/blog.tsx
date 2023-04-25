14941fb1532f69c1782feff5ff775c90a13e347b
hhshhhskk <hhshhhskk@naver.com>
2023-04-25 17:16:00 +0900

    MDX

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,26 +3,38 @@ import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import { PageProps, graphql } from "gatsby"
 
-const BlogPage = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
-    return (
-        <Layout title="Blog">
-            <ul>
-                {data.allFile.nodes.map((file, index) => (
-                    <li key={index}>{file.name}</li>
-                ))}
-            </ul>
-        </Layout>
-    )
+const BlogPage = ({ data }: PageProps<Queries.BlogPostsQuery>) => {
+  return (
+    <Layout title="Blog">
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article key={index}>
+            <h3>{file.frontmatter?.title}</h3>
+            <h5>{file.frontmatter?.author} in {file.frontmatter?.category}</h5>
+            <h6>{file.frontmatter?.date}</h6>
+            <hr />
+            <p>{file.excerpt}</p>
+          </article>
+        ))}
+      </section>
+    </Layout>
+  )
 }
 
 export const query = graphql`
-query BlogTitles {
-    allFile {
-      nodes{
-        name
+query BlogPosts {
+  allMdx {
+    nodes {
+      frontmatter {
+        category
+        date(formatString: "YYYY.MM.DD")
+        title
+        author
       }
+      excerpt(pruneLength: 50)
     }
   }
+}
 `;
 
 export const Head = () => <Seo title="Blog" />
