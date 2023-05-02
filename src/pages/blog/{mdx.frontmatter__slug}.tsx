bf5af03c85662d6a33da5516793031cc6bc0603a
hhshhhskk <hhshhhskk@naver.com>
2023-05-02 18:01:56 +0900

    gatsby-plugin-image

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { graphql } from "gatsby";
 
-export default function BlogPost() {
-    return
-    <Layout title="Blog Post">
-        <div></div>
-    </Layout>
+interface IBlogPostProps {
+    data: Queries.PostDetailQuery,
+    children: any,
 }
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
+    return (
+        <Layout title="">
+            <div>{children}</div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query PostDetail($frontmatter__slug: String) {
+        mdx(frontmatter: { slug: { eq: $frontmatter__slug}}){
+            body
+            frontmatter {
+                author
+                category
+                date
+                title
+                slug
+            }
+        }
+    }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout title="Welcome to DevStickers">
-      <div>
-      </div>
+      <StaticImage src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80" alt="Sticker on the wall" />
     </Layout>
   )
 }
